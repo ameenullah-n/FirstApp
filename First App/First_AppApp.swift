60e8a6a9c2d9c888a8b17a796361198535235985
Ameenullah Naseer <ameeen@me.com>
2026-01-12 20:17:59 +0530

    Initial Commit

---
+++ First App/First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppApp.swift
+//  First App
+//
+//  Created by Ameenullah Naseer on 12-1-26.
+//
+
+import SwiftUI
+
+@main
+struct First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
